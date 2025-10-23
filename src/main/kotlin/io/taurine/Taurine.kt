dc24e2420fed6488e3a0dae01f6397f4147f8385
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2025-10-23 23:25:34 +0200

    Initial split from packmod

---
+++ src/main/kotlin/io/taurine/Taurine.kt
@@ -0,0 +1,27 @@
+package io.taurine
+
+import net.neoforged.api.distmarker.Dist
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.common.Mod
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+@Mod(Taurine.ID)
+@EventBusSubscriber(value = [Dist.CLIENT])
+object Taurine {
+    const val ID = "taurine"
+
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
+    init {
+        LOGGER.log(Level.INFO, "Taurine/init")
+    }
+
+    @SubscribeEvent
+    fun onCommonSetup(event: FMLCommonSetupEvent) {
+        LOGGER.log(Level.INFO, "Taurine/setup")
+    }
+}
---
+++ src/main/resources/assets/taurine/instance/scaling_fluid.vert
@@ -0,0 +1,16 @@
+#include "flywheel:util/quaternion.glsl"
+#include "flywheel:util/matrix.glsl"
+
+void flw_instanceVertex(in FlwInstance instance) {
+    /*flw_vertexPos.x *= instance.size.x;
+    flw_vertexPos.y *= instance.size.y;
+    flw_vertexPos.z *= instance.size.z;*/
+    flw_vertexTexCoord.t = instance.v0 + flw_vertexPos.x * instance.vScale;
+    flw_vertexTexCoord.s = instance.u0 + flw_vertexPos.z * instance.uScale;
+
+    flw_vertexColor = instance.color;
+    flw_vertexPos = instance.pose * flw_vertexPos;
+    flw_vertexNormal = mat3(transpose(inverse(instance.pose))) * flw_vertexNormal;
+    flw_vertexOverlay = instance.overlay;
+    flw_vertexLight = max(vec2(instance.light) / 256., flw_vertexLight);
+}
