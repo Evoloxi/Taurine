8f938d57f9b2d78adf898975f711defbf01e4bc4
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2025-10-24 03:46:22 +0200

    Update comments

--- src/main/java/io/taurine/mixin/vanillin/ItemModelsMixin.java
+++ src/main/java/io/taurine/mixin/vanillin/ItemModelsMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Restriction(require = {
-        @Condition("continuity") // Three-way incompatibility with Continuity, MoonlightLib and Vanillin
+        @Condition("continuity")
 })
 @Mixin(ItemModels.class)
 public abstract class ItemModelsMixin {
@@ -23,6 +23,6 @@ public abstract class ItemModelsMixin {
             )
     )
     private static boolean onIsSupported(boolean original, @Local(name = "c") Class<? extends BakedModel> clazz) {
-        return original || clazz == EmissiveBakedModel.class; // Moonlight does some weird stuff that causes continuity to wrap every model in an EmissiveBakedModel (I think)
+        return original || clazz == EmissiveBakedModel.class; // Continuity wraps every model in an EmissiveBakedModel if there are any emissive textures ~Pepper
     }
 }
