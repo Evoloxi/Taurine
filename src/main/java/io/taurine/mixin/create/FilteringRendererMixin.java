1dda581cf421821b2261e8e13d507d2a5d83be3a
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2026-04-05 23:21:46 +0200

    VS compat

--- src/main/java/io/taurine/mixin/create/FilteringRendererMixin.java
+++ src/main/java/io/taurine/mixin/create/FilteringRendererMixin.java
@@ -7,17 +7,14 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.simibubi.create.foundation.blockEntity.behaviour.filtering.FilteringBehaviour;
 import com.simibubi.create.foundation.blockEntity.behaviour.filtering.FilteringRenderer;
 import io.taurine.ModelCache;
-import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = FilteringRenderer.class, remap = false)
+@Mixin(value = FilteringRenderer.class, remap = false, priority = 1300)
 public abstract class FilteringRendererMixin {
 
-    @Definition(id = "cameraEntity" , local  = @Local(type = Entity.class, name = "cameraEntity"))
-    @Definition(id = "position"     , method = "Lnet/minecraft/world/entity/Entity;position()Lnet/minecraft/world/phys/Vec3;")
-    @Definition(id = "distanceToSqr", method = "Lnet/minecraft/world/phys/Vec3;distanceToSqr(Lnet/minecraft/world/phys/Vec3;)D")
-    @Expression("cameraEntity.position().distanceToSqr(?) > ?")
+    @Definition(id = "max", local = @Local(type = float.class, name = "max"))
+    @Expression("? > (double) (max * max)")
     @ModifyExpressionValue(
             method = "renderOnBlockEntity",
             at = @At(value = "MIXINEXTRAS:EXPRESSION")
