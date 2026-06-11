d9e0281000c335b176545bc1873e0dda59a242d6
Evoloxi <69534332+Evoloxi@users.noreply.github.com>
2026-06-11 11:59:39 +0200

    half float matrix support

--- src/main/java/io/taurine/mixin/flywheel/InstanceAssemblerComponentMixin.java
+++ src/main/java/io/taurine/mixin/flywheel/InstanceAssemblerComponentMixin.java
@@ -2,10 +2,10 @@ package io.taurine.mixin.flywheel;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import dev.engine_room.flywheel.api.layout.ScalarElementType;
-import dev.engine_room.flywheel.api.layout.ValueRepr;
-import dev.engine_room.flywheel.api.layout.VectorElementType;
+import dev.engine_room.flywheel.api.layout.*;
 import dev.engine_room.flywheel.backend.compile.LayoutInterpreter;
 import dev.engine_room.flywheel.backend.compile.component.InstanceAssemblerComponent;
 import dev.engine_room.flywheel.backend.glsl.generate.GlslBlock;
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 @Mixin(InstanceAssemblerComponent.class)
 public class InstanceAssemblerComponentMixin implements HalfFloatCompatible {
@@ -87,8 +88,8 @@ public class InstanceAssemblerComponentMixin implements HalfFloatCompatible {
         }
     }
 
-    @Definition(id = "repr", method = "repr")
-    @Expression("? = ?.repr()")
+    @Definition(id = "size", method = "Ldev/engine_room/flywheel/api/layout/VectorElementType;size()I")
+    @Expression("? = ?.size()")
     @Inject(
             method = "unpackVector(Ldev/engine_room/flywheel/api/layout/VectorElementType;I)Ldev/engine_room/flywheel/backend/glsl/generate/GlslExpr;",
             at = @At(value = "MIXINEXTRAS:EXPRESSION", shift = At.Shift.AFTER),
@@ -97,14 +98,42 @@ public class InstanceAssemblerComponentMixin implements HalfFloatCompatible {
     private void unpackVector(
             VectorElementType type,
             int byteOffset,
-            CallbackInfoReturnable<GlslExpr> cir, @Local(name = "repr") ValueRepr repr
-    ) {
+            CallbackInfoReturnable<GlslExpr> cir,
+            @Local(name = "repr") ValueRepr repr,
+            @Local(name = "size") int size
+            ) {
         if (repr == TaurineFloatRepr.HALF_FLOAT) {
             int shortOffset = byteOffset / Short.BYTES;
-            cir.setReturnValue(taurine$unpackHalfFloatVector(LayoutInterpreter.vectorTypeName(type), type.size(), shortOffset, taurine$body));
+            cir.setReturnValue(taurine$unpackHalfFloatVector(LayoutInterpreter.vectorTypeName(type), size, shortOffset, taurine$body));
         }
     }
 
+    @WrapOperation(
+            method = "unpackMatrix",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ldev/engine_room/flywheel/backend/compile/component/InstanceAssemblerComponent;unpackVector(Ljava/lang/String;IIILjava/util/function/Function;)Ldev/engine_room/flywheel/backend/glsl/generate/GlslExpr;"
+            )
+    )
+    private GlslExpr unpackMatrix(
+            InstanceAssemblerComponent instance,
+            String outType,
+            int size, int byteOffset, int byteSize,
+            Function<GlslExpr, GlslExpr> unpackingFunc,
+            Operation<GlslExpr> original,
+            @Local(name = "repr") FloatRepr repr
+    ) {
+        if  (repr == TaurineFloatRepr.HALF_FLOAT) {
+            int shortOffset = byteOffset / byteSize;
+            return taurine$unpackHalfFloatVector(outType, size, shortOffset, taurine$body);
+        } else {
+            return original.call(instance, outType, size, byteOffset, byteSize, unpackingFunc);
+        }
+    }
+
+    /**
+     * @param body exists for ease of porting from non-mixin implementation
+     * */
     @Override
     public @NotNull GlslExpr taurine$unpackHalfFloatScalar(int shortOffset, @Nullable GlslBlock body) {
         int wordOffset = shortOffset / 2;
